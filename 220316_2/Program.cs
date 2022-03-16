07111d2641af0c814e8d5ace742c251df3bfadf3
candy <C110196104@nkust.edu.tw>
2022-03-16 16:13:21 +0800

    加入專案檔案。

---
+++ 220316_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _220316_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int score;
+            Console.Write("請輸入你的分數：");
+            score = Convert.ToInt32(Console.ReadLine());
+            string result = score >= 60 ? "及格" : "不及格";
+            Console.WriteLine("劉同學，你這次{0}" , result);
+            Console.ReadLine();
+        }
+    }
+}
